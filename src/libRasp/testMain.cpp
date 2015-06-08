98136e7b2b914d5307d8b3b2e421467ad16f02fc
Sheep from RPi <sheep@tele-scope.jp>
2015-06-08 07:11:28 +0000

    updatetestMain

--- src/libRasp/testMain.cpp
+++ src/libRasp/testMain.cpp
@@ -1,20 +1,20 @@
 #include <stdio.h>
-#include "raspSPI.h"
+#include "raspGPIO.h"
 
 int main(){
 	
 	gpio_init();
-	gpio_configure(22, GPIO_OUTPUT);
+	gpio_configure(2, GPIO_OUTPUT);
 	
 	while (1) {
 		
-		gpio_set(22);
+		gpio_set(2);
 		printf("off\n");
-		sleep(100);
+		sleep(1);
 		
-		gpio_clear(22);
+		gpio_clear(2);
 		printf("on\n");
-		
+		sleep(1);
 	}
 	
 	return 0;
