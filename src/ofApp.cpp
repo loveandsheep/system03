4b73e03ab372082d3d0dfe4fc5bc103804c4c8c6
Ovis aries <culminate0523@gmail.com>
2015-06-22 00:23:54 +0900

    comment

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -3,14 +3,14 @@
 //--------------------------------------------------------------
 void ofApp::setup(){
 
-//	fontPt.setup("font/Curveless.ttf", 0.5, "test");
-//	sys03.init();
-//	camera.setDistance(300);
-//	
-//	ofxRPiGPIO::setup();
-//#ifdef USE_GPIO
-//	ofxRPiGPIO::setPinMode(22, GPIO_OUTPUT);
-//#endif
+	fontPt.setup("font/Curveless.ttf", 0.5, "test");
+	sys03.init();
+	camera.setDistance(300);
+	
+	ofxRPiGPIO::setup();
+#ifdef USE_GPIO
+	ofxRPiGPIO::setPinMode(22, GPIO_OUTPUT);
+#endif
 }
 
 //--------------------------------------------------------------
@@ -18,9 +18,9 @@ void ofApp::update(){
 	
 //	fontPt.update();
 //	sys03.update(fontPt.getPoint());
-//	if (ofGetFrameNum() % 60 == 0) ofxRPiGPIO::setPinValue(22, true);
-//	if (ofGetFrameNum() % 60 == 30) ofxRPiGPIO::setPinValue(22, false);
-//	if (ofGetFrameNum() % 60 == 0) printf("Timer cout \n");
+	if (ofGetFrameNum() % 60 == 0) ofxRPiGPIO::setPinValue(22, true);
+	if (ofGetFrameNum() % 60 == 30) ofxRPiGPIO::setPinValue(22, false);
+	if (ofGetFrameNum() % 60 == 0) printf("Timer cout \n");
 }
 
 //--------------------------------------------------------------
@@ -28,11 +28,11 @@ void ofApp::draw(){
 
 	ofBackground(20);
 	
-//	camera.begin();
-//	fontPt.drawDebug();
-//	sys03.view();
-//	camera.end();
-//	
+	camera.begin();
+	fontPt.drawDebug();
+	sys03.view();
+	camera.end();
+	
 	
 	
 }
