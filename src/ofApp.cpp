32283952e67ff5e5d09ed8d9487ffbf71e2ede7d
Ovis aries <culminate0523@gmail.com>
2015-06-22 01:20:07 +0900

    enabled spi

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -30,10 +30,10 @@ void ofApp::draw(){
 
 	ofBackground(20);
 	
-//	camera.begin();
-//	fontPt.drawDebug();
-//	sys03.view();
-//	camera.end();
+	camera.begin();
+	fontPt.drawDebug();
+	sys03.view();
+	camera.end();
 	
 	
 	
