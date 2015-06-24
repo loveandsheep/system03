6159e0a082bc8ce31d2a6eaa9d7276f8d04ba08f
Ovis aries <culminate0523@gmail.com>
2015-06-24 19:34:31 +0900

    1

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -24,7 +24,7 @@ void ofApp::setup(){
 //--------------------------------------------------------------
 void ofApp::update(){
 //	fontPt.update();
-	if (ofGetFrameNum() % 3 == 0){
+	if (ofGetFrameNum() % 1 == 0){
 //		testMotor.enableAllMotor();
 		cout << "Send" << endl;
 		testMotor.sendSpi('a');
