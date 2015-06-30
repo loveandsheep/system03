c63c47523a4eea72af272ccb6b38a8cc5e84b5e9
Ovis aries <culminate0523@gmail.com>
2015-06-30 19:51:49 +0900

    test init

--- src/system03.cpp
+++ src/system03.cpp
@@ -19,13 +19,14 @@ void system03::init()
 	motor.resetDevice();
 	motor.resetDevice();
 	
+	sleep(1);
 	motor.enableAllMotor();
-//	motor.sendSignal(RPI_L6470_SIG_VOLT_RUN, 0xFF);
-//	motor.sendSignal(RPI_L6470_SIG_VOLT_ACC, 0xFF);
-//	motor.sendSignal(RPI_L6470_SIG_VOLT_HOLD, 0xFF);
-//	motor.sendSignal(RPI_L6470_SIG_MAXSPEED, 0x1);
-//	motor.sendSignal(RPI_L6470_SIG_ACCEL, 0x05);
-//	motor.sendSignal(RPI_L6470_SIG_DECEL, 0x05);
+	motor.sendSignal(RPI_L6470_SIG_VOLT_RUN, 0xFF);
+	motor.sendSignal(RPI_L6470_SIG_VOLT_ACC, 0xFF);
+	motor.sendSignal(RPI_L6470_SIG_VOLT_HOLD, 0xFF);
+	motor.sendSignal(RPI_L6470_SIG_MAXSPEED, 0x5);
+	motor.sendSignal(RPI_L6470_SIG_ACCEL, 0x05);
+	motor.sendSignal(RPI_L6470_SIG_DECEL, 0x05);
 	motor.sendSignal(RPI_L6470_SIG_ABSPOS, center);
 	motor.sendSignal(RPI_L6470_SIG_GOTO, center);
 	motor.sendSignal(RPI_L6470_SIG_STOP_HARD, 0);
