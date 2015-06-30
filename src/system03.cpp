6460f7290b8aff1e2a15e17d4b07fe069fa4d79d
Ovis aries <culminate0523@gmail.com>
2015-06-30 18:17:04 +0900

    motor flip

--- src/system03.cpp
+++ src/system03.cpp
@@ -44,9 +44,9 @@ void system03::update(const ofVec3f target)
 		sendDefaultPos();
 	}else{
 		motor.enableAllMotor();
-		motor_pos[0] = -eyes.arm[0].rootPan / 1.8f * 128;//1.8何？
+		motor_pos[0] =  eyes.arm[0].rootPan / 1.8f * 128;//1.8何？
 		motor_pos[1] = -eyes.arm[1].rootPan / 1.8f * 128;
-		motor_pos[2] = -eyes.arm[2].rootPan / 1.8f * 128;
+		motor_pos[2] =  eyes.arm[2].rootPan / 1.8f * 128;
 		motor.setGo_toMult(motor_pos);
 	}
 }
