7e940fe93d794a130693633d25e167284fc750d8
Ovis aries <culminate0523@gmail.com>
2015-06-30 19:40:32 +0900

    pin asign

--- src/system03.cpp
+++ src/system03.cpp
@@ -43,9 +43,9 @@ void system03::update(const ofVec3f target)
 		sendDefaultPos();
 	}else{
 		motor.enableAllMotor();
-		motor_pos[0] =  eyes.arm[0].rootPan / 1.8f * 128 + center;//1.8何？
+		motor_pos[0] =  eyes.arm[2].rootPan / 1.8f * 128 + center;
 		motor_pos[1] = -eyes.arm[1].rootPan / 1.8f * 128 + center;
-		motor_pos[2] =  eyes.arm[2].rootPan / 1.8f * 128 + center;
+		motor_pos[2] =  eyes.arm[0].rootPan / 1.8f * 128 + center;
 		motor.setGo_toMult(motor_pos);
 	}
 }
