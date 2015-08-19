aba7a789a50ba923be2f2cb77395d755f051cebf
Ovis aries <culminate0523@gmail.com>
2015-08-19 15:34:46 +0900

    ch fix

--- src/system03.cpp
+++ src/system03.cpp
@@ -51,8 +51,8 @@ void system03::update(const ofVec3f target)
 			motor_pos_prev[i] = motor_pos[i];
 		
 		motor_pos[0] = -eyes.arm[0].rootPan / 1.8f * 128 + center;
-		motor_pos[1] = -eyes.arm[1].rootPan / 1.8f * 128 + center;
-		motor_pos[2] = -eyes.arm[2].rootPan / 1.8f * 128 + center;
+		motor_pos[1] = -eyes.arm[2].rootPan / 1.8f * 128 + center;
+		motor_pos[2] = -eyes.arm[1].rootPan / 1.8f * 128 + center;
 		
 		//移動量に応じて速度を調整
 //		for (int i = 0;i < motor_pos.size();i++)
