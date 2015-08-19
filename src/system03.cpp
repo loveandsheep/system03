de33ce7500fd883668ec89757e8490f5404825e6
Ovis aries <culminate0523@gmail.com>
2015-08-19 16:10:52 +0900

    voltage

--- src/system03.cpp
+++ src/system03.cpp
@@ -22,9 +22,9 @@ void system03::init()
 	motor.sendSignal(RPI_L6470_SIG_ACCEL, 0x10);
 	motor.sendSignal(RPI_L6470_SIG_DECEL, 0x10);
 	motor.sendSignal(RPI_L6470_SIG_MAXSPEED, 0x40);
-	motor.sendSignal(RPI_L6470_SIG_VOLT_RUN, 0xFF);
-	motor.sendSignal(RPI_L6470_SIG_VOLT_ACC, 0xFF);
-	motor.sendSignal(RPI_L6470_SIG_VOLT_HOLD, 0xFF);
+	motor.sendSignal(RPI_L6470_SIG_VOLT_RUN, 0x80);
+	motor.sendSignal(RPI_L6470_SIG_VOLT_ACC, 0x80);
+	motor.sendSignal(RPI_L6470_SIG_VOLT_HOLD, 0x80);
 	motor.sendSignal(RPI_L6470_SIG_ABSPOS, center);
 	motor.sendSignal(RPI_L6470_SIG_GOTO, center);
 	motor.sendSignal(RPI_L6470_SIG_STOP_HARD, 0);
